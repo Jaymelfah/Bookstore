c2ec2228abf519e9a908cb389d8282fe4cdf4569
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-11-01 15:13:04 +0000

    Initialize all components needed for bookstore website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import CategoriesPage from './components/CategoriesPage';
+import BookPage from './components/BookPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<BookPage />} />
+        <Route exact path="/categories" element={<CategoriesPage />} />
+      </Routes>
+    </>
   );
 }
 
