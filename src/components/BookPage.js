9ad11281893cc796d0bc4d15416257bbc592b0de
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-11-08 06:00:37 +0000

    clean code for book page

--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -5,24 +5,6 @@ import Form from './Form';
 
 const BookPage = () => {
   const books = useSelector((state) => state.book);
-  // const allbooks = [
-  //   {
-  //     id: 1,
-  //     title: 'A Tale of two Cities',
-  //     author: 'Charles Dickens',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'In the name of the Wind',
-  //     author: 'Patrick Rothfuss',
-  //   },
-  //   {
-  //     id: 3,
-  //     title: 'A Song of Ice and Fire',
-  //     author: 'George R. R. Martin',
-  //   },
-  // ];
-
   return (
     <>
       <Booklist books={books} />
